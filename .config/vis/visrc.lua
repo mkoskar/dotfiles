3a58ff7fb404a2d1c5d43f435070530d70c77425
Miroslav Koškár <mk@mkoskar.com>
2018-02-27 00:17:42 +0100

    Updates

--- .config/vis/visrc.lua
+++ .config/vis/visrc.lua
@@ -9,7 +9,11 @@ vis.events.subscribe(vis.events.INIT, function()
     vis:command('set expandtab')
     vis:command('set shell bashx')
     vis:command('set tabwidth 4')
-    vis:command('set theme default-256')
+    vis:command('set theme custom')
+
+
+    -- Moving Around, Searching and Patterns
+    ----------------------------------------
 
     vis:command('map! normal <M-j> 5j')
     vis:command('map! normal <M-k> 5k')
@@ -21,12 +25,25 @@ vis.events.subscribe(vis.events.INIT, function()
     vis:command('map! normal <C-e> 5<vis-window-slide-down>')
     vis:command('map! normal <C-y> 5<vis-window-slide-up>')
 
+
+    -- Displaying Text
+    ----------------------------------------
+
     vis:map(modes.NORMAL, ',L', function(keys)
         vis:command('set show-newlines!')
         vis:command('set show-spaces!')
+        vis:command('set show-tabs!')
     end, 'Toggle show-newlines and show-spaces')
 
-    vis:command('map! normal ,cul ":set cursorline!<Enter>"')
+
+    -- Syntax, Highlighting and Spelling
+    ----------------------------------------
+
+    vis:command('map! normal cul ":set cursorline!<Enter>"')
+
+
+    -- Multiple Windows
+    ----------------------------------------
 
     vis:command('map! normal <C-j> <C-w>j')
     vis:command('map! normal <C-k> <C-w>k')
@@ -34,14 +51,91 @@ vis.events.subscribe(vis.events.INIT, function()
     vis:command('map! normal <C-h> <C-w>h')
     vis:command('map! normal <Backspace> <C-h>')
 
+    vis:command('map! normal c<C-j> <C-w>s')
+    vis:command('map! normal c<C-k> <C-w>s<C-w>j')
+    vis:command('map! normal c<C-l> <C-w>v')
+    vis:command('map! normal c<C-h> <C-w>v<C-w>l')
+    vis:command('map! normal c<Backspace> c<C-h>')
+
+    vis:command('map! normal d<C-j> <C-w>j<C-w>c')
+    vis:command('map! normal d<C-k> <C-w>k<C-w>c')
+    vis:command('map! normal d<C-l> <C-w>l<C-w>c')
+    vis:command('map! normal d<C-h> <C-w>h<C-w>c')
+    vis:command('map! normal d<Backspace> d<C-h>')
+
     vis:command('map! normal ,q :q<Enter>')
+
+
+    -- Editing Text
+    ----------------------------------------
+
+    vis:command('map! insert <C-b> <Home>')
+    vis:command('map! insert <C-e> <End>')
+    vis:command('map! insert <M-h> <Left>')
+    vis:command('map! insert <M-l> <Right>')
+    vis:command('map! insert <M-b> <S-Left>')
+    vis:command('map! insert <M-f> <S-Right>')
+    vis:command('map! insert <M-w> <S-Right>')
+
+    -- <C-u>
+    vis:command('map! insert <C-k> <vis-operator-delete>$')
+    -- <C-w>
+    vis:command('map! insert <M-d> <vis-operator-delete>w')
+    vis:command('map! insert <M-x> <vis-delete-char-next>')
+
+    --vis:command('map! insert <C-l>')
+    vis:command('map! insert <M-j> <vis-motion-screenline-down>')
+    vis:command('map! insert <M-k> <vis-motion-screenline-up>')
+    vis:command('map! insert <C-n> <Down>')
+    vis:command('map! insert <C-p> <Up>')
+
+
+    -- Various
+    ----------------------------------------
+
+    vis:command('map! normal QQ :qa<Enter>')
+    vis:command('map! normal QA :qa!<Enter>')
     vis:command('map! normal ,D :%d<Enter>')
 
+    -- Open files faster
     vis:command('map! normal ,ew ":e "')
     vis:command('map! normal ,es ":sp "')
     vis:command('map! normal ,ev ":vsp "')
     vis:command('map! normal ,ee :e<Enter>')
     vis:command('map! normal ,E :e!<Enter>')
+
+    -- Read / Filter faster
+    vis:command('map! normal ,ri :<')
+    vis:command('map! visual | :|')
+
+    -- Sorting
+    vis:command('map! visual ,ss :|sort<Enter>')
+    vis:command('map! visual ,su ":|sort -u<Enter>"')
+    vis:command('map! visual ,sr ":|sort -r<Enter>"')
+    vis:command('map! visual ,sb :|sort-blocks<Enter>')
+
+    -- Format
+    vis:command('map! visual ,fo :|format<Enter>')
+    vis:command('map! normal ,fo :%|format<Enter>')
+
+    -- XML prettify
+    vis:command('map! visual ,px ":|format xml<Enter>"')
+    vis:command('map! normal ,px ":%|format xml<Enter>"')
+
+    -- Strip / Squash lines
+    vis:command('map! visual ,ps :|striplns<Enter>')
+    vis:command('map! normal ,ps :%|striplns<Enter>')
+    vis:command('map! visual ,pS :|squashlns<Enter>')
+    vis:command('map! normal ,pS :%|squashlns<Enter>')
+
+    -- URLs
+    vis:command('map! visual ,uo ":>urls -o<Enter>"')
+    vis:command('map! normal ,uo ":>urls -o<Enter>"')
+    vis:command('map! normal ,uO ":%>urls -o<Enter>"')
+
+    vis:command('map! visual ,uy ":>urls -y<Enter>"')
+    vis:command('map! normal ,uy ":>urls -y<Enter>"')
+    vis:command('map! normal ,uY ":%>urls -y<Enter>"')
 
 end)
 
@@ -49,6 +143,5 @@ vis.events.subscribe(vis.events.WIN_OPEN, function(win)
 
     vis:command('set colorcolumn 80')
     vis:command('set show-eof')
-    vis:command('set show-tabs')
 
 end)
